5d7ac77f64922a9ce16cd48f911d994d7af85742
Michael Winser <mike.winser@gmail.com>
2021-08-13 13:05:02 -0700

    Finished Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage 
+          alt="Person standing below a huge starlit night sky"
+          src="../images/stars.jpg"
+        />
       </Layout>
   )
 }
