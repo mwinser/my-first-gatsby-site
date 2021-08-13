b608812acbf2608f860d8e88e6c52b6f935d0bf7
Michael Winser <mike.winser@gmail.com>
2021-08-13 13:51:23 -0700

    GraphQL Tut Part 4 complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
   ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,34 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
     container,
     heading,
     navLinks,
     navLinkItem,
-    navLinkText
+    navLinkText,
+    siteTitle
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+    query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+      
+    `)
+    console.log(data)
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+          <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
           <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
         </ul>
       </nav>
