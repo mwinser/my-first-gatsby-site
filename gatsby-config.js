0836a3a97ec0cdc6087887deb12d4f8d8d86220d
Michael Winser <mike.winser@gmail.com>
2021-08-13 18:48:40 -0700

    MDX blog posts Tutorial complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     {
       resolve: "gatsby-source-filesystem",
       options: {
